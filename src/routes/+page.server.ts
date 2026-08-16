60e2b8e788e3e66352aab6ac80c0594b3ece7749
Miguel Rivero <8478733+RMiguelRivero@users.noreply.github.com>
2026-08-16 23:00:55 +0200

    Check locale in cookie before deaulting to default_locale for '/'

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { resolve } from '$app/paths';
+import { toPathname } from '$lib/utils/toPathname';
+import { DEFAULT_LOCALE, LOCALE_COOKIE_NAME } from '$lib/i18n/constants';
+import { resolveFallbackLocale } from '$lib/i18n/utils';
 
-export const load: PageServerLoad = () => {
-	throw redirect(307, '/en');
+export const load: PageServerLoad = ({ cookies }) => {
+	const locale = resolveFallbackLocale(cookies.get(LOCALE_COOKIE_NAME)) ?? DEFAULT_LOCALE;
+	redirect(307, resolve(toPathname(`/${locale}`)));
 };
